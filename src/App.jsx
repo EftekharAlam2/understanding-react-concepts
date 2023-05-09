341b8923ab4eb436a25b1f5f92ca844163297f04
EftekharAlam2 <hinsouvlmare@gmail.com>
2023-05-09 18:13:46 +0600

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 import "./App.css";
-import Device from "./components/Device";
-import Watch from "./components/Watch";
-import Knob from "./components/Knob";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <Knob steps="456"></Knob>
-      <Device name="uphone" price="13000"></Device>
-      <Device name="Samsing" price="11000"></Device>
-      <Watch></Watch>
-      <h1>Vite + React</h1>
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -22,7 +25,10 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-    </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   );
 }
 
