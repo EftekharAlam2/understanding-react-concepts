a8ccb3eefed48fd7c0c6f76c112f4c6aeaa6e4b3
EftekharAlam2 <hinsouvlmare@gmail.com>
2023-05-09 18:09:38 +0600

    layout added

---
+++ src/components/Device.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Device = () => {
+  return (
+    <div>
+      <h1>device</h1>
+    </div>
+  );
+};
+
+export default Device;
---
+++ src/components/Knob.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Knob = () => {
+  return (
+    <div>
+      <h1>knob</h1>
+    </div>
+  );
+};
+
+export default Knob;
